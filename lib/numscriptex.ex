daf0a43996b45076fb6527fb9c951de5842be180
Vinicius <vinicius.alexandre68@gmail.com>
2024-12-03 14:44:54 -0300

    First commit

---
+++ lib/numscriptex.ex
@@ -0,0 +1,18 @@
+defmodule Numscriptex do
+  @moduledoc """
+  Documentation for `Numscriptex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Numscriptex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/numscriptex_test.exs
@@ -0,0 +1,8 @@
+defmodule NumscriptexTest do
+  use ExUnit.Case
+  doctest Numscriptex
+
+  test "greets the world" do
+    assert Numscriptex.hello() == :world
+  end
+end
