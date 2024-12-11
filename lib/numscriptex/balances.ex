d5ab7b2f4286ca0bed8e670a37d9bbb85a0c59af
Vinicius <vinicius.alexandre68@gmail.com>
2024-12-11 16:32:04 -0300

    Create Balances module tests

--- lib/numscriptex/balances.ex
+++ lib/numscriptex/balances.ex
@@ -2,7 +2,7 @@ defmodule Numscriptex.Balances do
   @spec put(map(), list()) :: list(map())
   def put(initial_balance, postings) do
     initial_balance
-    |> Enum.map(fn {account, assets} -> 
+    |> Enum.map(fn {account, assets} ->
       Enum.map(assets, fn {asset, amount} ->
         %{
           "account" => account,
@@ -13,20 +13,20 @@ defmodule Numscriptex.Balances do
       end)
     end)
     |> List.flatten()
-    |> Enum.map(fn balance_data -> 
-      Enum.reduce(postings, {%{}, 0}, fn posting, {_map, acc} -> 
+    |> Enum.map(fn balance_data ->
+      Enum.reduce(postings, {%{}, 0}, fn posting, {_map, acc} ->
         {
           balance_data,
           handle_final_balance(balance_data, posting, acc)
         }
       end)
     end)
-    |> Enum.map(fn {balance, final_balance} -> 
+    |> Enum.map(fn {balance, final_balance} ->
       %{balance | "final_balance" => final_balance}
     end)
   end
 
-  def handle_final_balance(balance_data, posting, acc) do
+  defp handle_final_balance(balance_data, posting, acc) do
     same_asset? = balance_data["asset"] == posting["asset"]
     source? = posting["source"] === balance_data["account"]
     destination? = posting["destination"] === balance_data["account"]
@@ -35,10 +35,12 @@ defmodule Numscriptex.Balances do
 
     cond do
       source? and same_asset? ->
-        (balance - posting["amount"]) - acc
-      destination? and same_asset? -> 
+        balance - posting["amount"] - acc
+
+      destination? and same_asset? ->
         balance + posting["amount"]
-      true -> 
+
+      true ->
         acc
     end
   end
