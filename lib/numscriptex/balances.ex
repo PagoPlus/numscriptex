f886ad3fdaeb00962f8fee1d74857fbbc5ca451d
Vinicius <vinicius.alexandre68@gmail.com>
2024-12-13 13:57:20 -0300

    Fix Balances

--- lib/numscriptex/balances.ex
+++ lib/numscriptex/balances.ex
@@ -1,23 +1,63 @@
 defmodule Numscriptex.Balances do
   @spec put(map(), list()) :: list(map())
-  def put(initial_balance, postings) do
-    initial_balance
-    |> Enum.map(fn {account, assets} ->
-      Enum.map(assets, fn {asset, amount} ->
+  def put(account_assets, postings) do
+    account_assets
+    |> build_balances(postings)
+    |> Enum.uniq()
+    |> handle_initial_balance(account_assets)
+    |> handle_final_balance(postings)
+  end
+
+  defp build_balances(account_assets, postings) do
+    balances_by_assets = build_balances_by_assets(account_assets)
+    balances_by_postings = build_balances_by_postings(postings)
+
+    balances_by_assets ++ balances_by_postings
+  end
+
+  defp build_balances_by_assets(account_assets) do
+    Enum.flat_map(account_assets, fn {account, assets} ->
+      Enum.map(assets, fn {asset, _amount} ->
         %{
           "account" => account,
           "asset" => asset,
-          "initial_balance" => amount,
-          "final_balance" => amount
+          "initial_balance" => 0,
+          "final_balance" => 0
         }
       end)
     end)
-    |> List.flatten()
-    |> Enum.map(fn balance_data ->
-      Enum.reduce(postings, {%{}, 0}, fn posting, {_map, acc} ->
+  end
+
+  defp build_balances_by_postings(postings) do
+    Enum.map(postings, fn posting ->
+        %{
+          "account" => posting["destination"],
+          "asset" => posting["asset"],
+          "initial_balance" => 0,
+          "final_balance" => 0
+        }
+    end)
+  end
+
+  defp handle_initial_balance(balances, account_assets) do
+    Enum.map(balances, fn balance -> 
+      account = balance["account"]
+      asset = balance["asset"]
+
+      initial_balance = account_assets[account][asset] || 0
+
+      %{balance | "initial_balance" => initial_balance}
+    end)
+  end
+
+  defp handle_final_balance(balances, postings) do
+    Enum.map(balances, fn balance ->
+      initial_balance = balance["initial_balance"]
+
+      Enum.reduce(postings, {%{}, initial_balance}, fn posting, {_map, acc} ->
         {
-          balance_data,
-          handle_final_balance(balance_data, posting, acc)
+          balance,
+          calculate_final_balance(balance, posting, acc)
         }
       end)
     end)
@@ -26,22 +66,20 @@ defmodule Numscriptex.Balances do
     end)
   end
 
-  defp handle_final_balance(balance_data, posting, acc) do
-    same_asset? = balance_data["asset"] == posting["asset"]
-    source? = posting["source"] === balance_data["account"]
-    destination? = posting["destination"] === balance_data["account"]
-
-    balance = balance_data["final_balance"]
+  defp calculate_final_balance(balance, posting, initial_balance) do
+    same_asset? = posting["asset"] == balance["asset"]
+    source? = posting["source"] === balance["account"]
+    destination? = posting["destination"] === balance["account"]
 
     cond do
       source? and same_asset? ->
-        balance - posting["amount"] - acc
+        initial_balance - posting["amount"]
 
       destination? and same_asset? ->
-        balance + posting["amount"]
+        initial_balance + posting["amount"]
 
       true ->
-        acc
+        initial_balance
     end
   end
 end
