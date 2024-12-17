42796e7f4481210adbadb262d16a6de6712cefc0
Vinicius <vinicius.alexandre68@gmail.com>
2024-12-17 16:37:35 -0300

    Create new alias

--- mix.exs
+++ mix.exs
@@ -7,9 +7,10 @@ defmodule Numscriptex.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [coveralls: :test]
+      preferred_cli_env: [coveralls: :test, ci: :test]
     ]
   end
 
@@ -26,6 +27,17 @@ defmodule Numscriptex.MixProject do
       {:wasmex, "~> 0.9.2"},
       {:excoveralls, "~> 0.18", only: :test},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      ci: [
+        "format --check-formatted",
+        "deps.unlock --check-unused",
+        "credo suggest --all --ignore design",
+        "test"
+      ]
     ]
   end
 end
