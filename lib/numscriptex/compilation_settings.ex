b23b20477c6fd4e538317cf25d9ce0af5f70cfd4
Vinicius <vinicius.alexandre68@gmail.com>
2025-01-16 15:27:44 -0300

    Improve error handling

--- lib/numscriptex/compilation_settings.ex
+++ lib/numscriptex/compilation_settings.ex
@@ -2,43 +2,72 @@ defmodule Numscriptex.CompilationSettings do
   @moduledoc false
 
   @file_version Application.compile_env(:numscriptex, :file_version, "0.23.4")
+  # @url "https://github.com/PagoPlus/numscriptex/releases/download/v#{@file_version}/numscript-wasm_v#{@file_version}_Wasip1_wasm.tar.gz"
   @url "https://github.com/yarnpkg/yarn/releases/download/v#{@file_version}/yarn-v#{@file_version}.tar.gz"
   @download_path System.tmp_dir()
+                 # |> Path.join("numscript-wasm_v#{@file_version}_Wasip1_wasm.tar.gz")
                  |> Path.join("yarn-v#{@file_version}.tar.gz")
                  |> String.to_charlist()
 
   defmacro ensure_wasm_file_exists do
+    quote do: unquote(download_and_extract_wasm_file())
+  end
+
+  defp download_and_extract_wasm_file() do
     quote do
-      file_path =
-        :numscriptex
-        |> :code.priv_dir()
-        |> Path.join("dist")
-
-      if File.exists?(file_path) do
-        Logger.info("WASM file already exists.")
-      else
-        Logger.info("Downloading numscript-wasm.tar.gz")
-        unquote(download_wasm_file())
-        Logger.info("numscript-wasm.tar.gz downloaded")
-
-        Logger.info("Extracting numscript-wasm.tar.gz")
-
-        :erl_tar.extract(unquote(@download_path), [
-          {:cwd, :code.priv_dir(:numscriptex)},
-          :compressed
-        ])
-
-        Logger.info("File successfully extracted on priv directory.")
-      end
+      download_result = unquote(download_wasm_file())
+      
+      with :ok <- download_result, do: unquote(extract_wasm_file())
     end
   end
 
   defp download_wasm_file do
     quote do
-      download_path = unquote(@download_path)
+      Logger.info("Downloading numscript-wasm.tar.gz")
 
-      {:ok, :saved_to_file} =
-        :httpc.request(:get, {unquote(@url), []}, [], stream: download_path)
+      case :httpc.request(:get, {unquote(@url), []}, [], stream: unquote(@download_path)) do
+        {:ok, :saved_to_file} ->
+          Logger.info("numscript-wasm.tar.gz downloaded")
+
+          :ok
+        {:ok, {{_, status_code, detail}, _, _}} when status_code not in 200..299 ->
+          Logger.error("Download request failed with status code #{status_code} #{detail}.")
+
+          :error
+
+        {:error, reason} ->
+          Logger.error("Failed to download numscript-wasm. Reason: #{reason}.")
+
+          :error
+      end
+    end
+  end
+
+  defp extract_wasm_file() do
+    quote do
+      Logger.info("Extracting numscript-wasm.tar.gz")
+
+      extraction_result = :erl_tar.extract(unquote(@download_path), [
+        {:cwd, :code.priv_dir(:numscriptex)},
+        :compressed
+      ])
+
+      case extraction_result do
+        :ok ->
+          Logger.info("File successfully extracted on priv directory.")
+
+          :ok
+
+        {:ok, _} ->
+          Logger.info("File successfully extracted on priv directory.")
+
+          :ok
+
+        {:error, reason} ->
+          Logger.error("Failed to extract the tar.gz file. Reason: #{inspect(reason)}.")
+
+          :error
+      end
     end
   end
 end
