3c4c853394666e5f3c90af2c57dbb99355ecff03
Vinicius <vinicius.alexandre68@gmail.com>
2025-02-20 16:52:33 -0300

    Create 'Posting' struct

---
+++ test/numscriptex/posting_tests.exs
@@ -0,0 +1,58 @@
+defmodule Numscriptex.PostingTest do
+  use ExUnit.Case
+
+  alias Numscriptex.Posting
+
+  doctest Numscriptex.Posting
+
+  describe "from_map/1" do
+    setup_all do
+      postings = [
+        %{
+          amount: 100,
+          asset: "USD/2",
+          destination: "bar",
+          source: "foo"
+        },
+        %{
+          amount: 100,
+          asset: "USD/2",
+          destination: "baz",
+          source: "foo"
+        }
+      ]
+
+      %{postings: postings}
+    end
+
+    test "transform a list of maps into a list of Posting structs", %{postings: list} do
+      assert postings = Posting.from_list(list)
+
+      assert postings == [
+               %Numscriptex.Posting{
+                 amount: 100,
+                 asset: "USD/2",
+                 destination: "bar",
+                 source: "foo"
+               },
+               %Numscriptex.Posting{
+                 amount: 100,
+                 asset: "USD/2",
+                 destination: "baz",
+                 source: "bar"
+               }
+             ]
+    end
+
+    test "transform a map into a Posting struct", %{postings: [map | _]} do
+      assert posting = Posting.from_map(map)
+
+      assert posting == %Numscriptex.Posting{
+               amount: 100,
+               asset: "USD/2",
+               destination: "bar",
+               source: "foo"
+             }
+    end
+  end
+end
