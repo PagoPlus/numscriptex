79c03907deac51cb346e81529c727aed8558c819
Vinicius <vinicius.alexandre68@gmail.com>
2025-02-25 17:15:02 -0300

    Create Builder module

---
+++ test/numscriptex/builder_test.exs
@@ -0,0 +1,197 @@
+defmodule Numscriptex.BuilderTest do
+  use ExUnit.Case
+
+  alias Numscriptex.Builder
+
+  doctest Numscriptex.Builder
+
+  describe "build/1" do
+    test "simple send with fixed value" do
+      metadata = %{
+        splits: [
+          %{
+            type: :fixed,
+            amount: 5,
+            asset: "USD/2",
+            account: "some:destination"
+          }
+        ]
+      }
+
+      assert {:ok, %{script: script}} = Builder.build(metadata)
+      assert {:ok, _script} = Numscriptex.check(script)
+
+      assert script == """
+             send [USD/2 500] (
+               source = @user
+               destination = @some:destination
+             )
+             """
+    end
+
+    test "simple send with percentage value" do
+      metadata = %{
+        splits: [
+          %{
+            type: :percent,
+            amount: 20,
+            asset: "USD/2",
+            account: "some:destination"
+          }
+        ],
+        percent_asset: "USD/2"
+      }
+
+      assert {:ok, %{script: script}} = Builder.build(metadata)
+      assert {:ok, _script} = Numscriptex.check(script)
+
+      assert script == """
+             send [USD/2 *] (
+               source = @user
+               destination = {
+                 20% to @some:destination
+                 remaining kept
+               }
+             )
+             """
+    end
+
+    test "multiple send with both fixed and percentage value with 'remaining_to' opt" do
+      metadata = %{
+        splits: [
+          %{
+            type: :percent,
+            amount: 20,
+            account: "some:destination:a"
+          },
+          %{
+            type: :fixed,
+            amount: 10,
+            asset: "USD/2",
+            account: "some:destination:b"
+          }
+        ],
+        percent_asset: "USD/2",
+        remaining_to: "another:destination"
+      }
+
+      assert {:ok, %{script: script}} = Builder.build(metadata)
+      assert {:ok, _script} = Numscriptex.check(script)
+
+      assert script == """
+             send [USD/2 *] (
+               source = @user
+               destination = {
+                 20% to @some:destination:a
+                 remaining to @another:destination
+               }
+             )
+             send [USD/2 1000] (
+               source = @user
+               destination = @some:destination:b
+             )
+             """
+    end
+
+    test "multiple send with nested destinations and fixed values and 'remaining_to' opt" do
+      metadata = %{
+        splits: [
+          %{
+            type: :fixed,
+            amount: 10.5,
+            asset: "EUR/2",
+            account: "some:destination:a"
+          },
+          %{
+            type: :percent,
+            amount: 20,
+            account: "some:destination:b"
+          },
+          %{
+            type: :percent,
+            amount: 30,
+            remaining_to: "remaining:destination:b",
+            splits: [
+              %{
+                type: :fixed,
+                amount: 12.5,
+                asset: "USD/2",
+                account: "some:destination:c"
+              },
+              %{
+                type: :percent,
+                amount: 15,
+                account: "some:destination:e"
+              },
+              %{
+                type: :percent,
+                amount: 20,
+                account: "some:destination:d"
+              },
+              %{
+                type: :percent,
+                amount: 50,
+                remaining_to: "remaining:destination:c",
+                splits: [
+                  %{
+                    type: :fixed,
+                    amount: 5,
+                    asset: "BRL/2",
+                    account: "some:destination:b"
+                  },
+                  %{
+                    type: :percent,
+                    amount: 20,
+                    account: "some:destination:a"
+                  },
+                  %{
+                    type: :percent,
+                    amount: 27,
+                    account: "some:destination:a"
+                  }
+                ]
+              }
+            ]
+          }
+        ],
+        remaining_to: "remaining:destination:a",
+        percent_asset: "EUR/2"
+      }
+
+      assert {:ok, %{script: script}} = Builder.build(metadata)
+      assert {:ok, _script} = Numscriptex.check(script)
+
+      assert script == """
+             send [EUR/2 *] (
+               source = @user
+               destination = {
+                 20% to @some:destination:b
+                 30% to {
+                 15% to @some:destination:e
+                 20% to @some:destination:d
+                 50% to {
+                 20% to @some:destination:a
+                 27% to @some:destination:a
+                   remaining to @remaining:destination:c
+                 }
+                   remaining to @remaining:destination:b
+                 }
+                 remaining to @remaining:destination:a
+               }
+             )
+             send [BRL/2 500] (
+               source = @user
+               destination = @some:destination:b
+             )
+             send [USD/2 1250] (
+               source = @user
+               destination = @some:destination:c
+             )
+             send [EUR/2 1050] (
+               source = @user
+               destination = @some:destination:a
+             )
+             """
+    end
+  end
+end
