1390884275b289db82e4655ec6c2f720b38f5d6a
Vinicius <vinicius.alexandre68@gmail.com>
2025-02-27 12:54:09 -0300

    Displays decimal values on %Posting{} and %Balance{}

--- lib/numscriptex/posting.ex
+++ lib/numscriptex/posting.ex
@@ -3,8 +3,11 @@ defmodule Numscriptex.Posting do
   `Numscriptex.Postings` represents a financial transaction made with [Numscript](https://docs.formance.com/numscript/)
   """
 
+  alias Numscriptex.Utilities
+
   @derive JSON.Encoder
   defstruct amount: nil,
+            decimal_amount: nil,
             asset: nil,
             destination: nil,
             source: nil
@@ -17,21 +20,37 @@ defmodule Numscriptex.Posting do
   * `:asset` the asset were the transaction was made
   * `:destination` account whose the money will go to
   * `:amount` amount of money transferred (integer)
+  * `:decimal_amount` amount of money transferred, but as float
   """
   @type t() :: %__MODULE__{
           amount: pos_integer(),
+          decimal_amount: float(),
           asset: bitstring(),
           destination: bitstring(),
           source: bitstring()
         }
 
-  @spec from_list(map()) :: list(__MODULE__.t())
+  @spec from_list(list(map())) :: list(__MODULE__.t())
   def from_list(postings) do
     Enum.map(postings, &from_map/1)
   end
 
-  @spec from_list(map()) :: __MODULE__.t()
-  def from_map(map) do
-    struct(%__MODULE__{}, map)
+  @spec from_map(map()) :: __MODULE__.t()
+  def from_map(posting) do
+    put_decimal_amount(posting)
+  end
+
+  defp put_decimal_amount(posting) when not is_struct(posting) do
+    posting
+    |> Map.put(:decimal_amount, 0)
+    |> then(fn elem -> struct(%__MODULE__{}, elem) end)
+    |> put_decimal_amount()
+  end
+
+  defp put_decimal_amount(posting) do
+    decimal_places = Utilities.decimal_places_from_asset(posting.asset)
+    decimal_amount = Utilities.integer_to_decimal(posting.amount, decimal_places)
+
+    %{posting | decimal_amount: decimal_amount}
   end
 end
