a0cc712f42312d4516aeea36a383e58dcd80b4bc
Vinicius <vinicius.alexandre68@gmail.com>
2025-04-15 14:10:07 -0300

    increase test coverage

--- test/numscriptex/builder_test.exs
+++ test/numscriptex/builder_test.exs
@@ -193,5 +193,300 @@ defmodule Numscriptex.BuilderTest do
              )
              """
     end
+
+    test "ignores account if split has nested splits" do
+      metadata = %{
+        splits: [
+          %{
+            type: :percent,
+            amount: 50,
+            account: "ignored:account",
+            remaining_to: "remaining:destination",
+            splits: [
+              %{
+                type: :percent,
+                amount: 20,
+                account: "some:destination"
+              }
+            ]
+          }
+        ],
+        percent_asset: "EUR",
+        remaining_to: "remaining:destination:a"
+      }
+
+      assert {:ok, %{script: script}} = Builder.build(metadata)
+      assert {:ok, _script} = Numscriptex.check(script)
+
+      assert script == """
+             send [EUR *] (
+               source = @user
+               destination = {
+                 50% to {
+                 20% to @some:destination
+                   remaining to @remaining:destination
+                 }
+                 remaining to @remaining:destination:a
+               }
+             )
+             """
+    end
+
+    test "if missing remaining_to, default to 'remaining kept'" do
+      metadata = %{
+        splits: [
+          %{
+            type: :percent,
+            amount: 50,
+            # missing remaining_to
+            splits: [
+              %{
+                type: :percent,
+                amount: 20,
+                account: "some:destination"
+              }
+            ]
+          }
+        ],
+        percent_asset: "EUR"
+        # missing remaining_to
+      }
+
+      assert {:ok, %{script: script}} = Builder.build(metadata)
+      assert {:ok, _script} = Numscriptex.check(script)
+
+      assert script == """
+             send [EUR *] (
+               source = @user
+               destination = {
+                 50% to {
+                 20% to @some:destination
+                   remaining kept
+                 }
+                 remaining kept
+               }
+             )
+             """
+    end
+
+    test "returns error when split type is invalid" do
+      metadata = %{
+        splits: [
+          %{
+            type: :invalid,
+            amount: 20,
+            account: "some:destination"
+          }
+        ],
+        remaining_to: "remaining:destination",
+        percent_asset: "EUR"
+      }
+
+      assert {:error, "Missing key on metadata."} = Builder.build(metadata)
+    end
+
+    test "returns error when splits key is missing" do
+      metadata = %{
+        remaining_to: "remaining:destination",
+        percent_asset: "EUR"
+      }
+
+      assert {:error, "Missing key on metadata."} = Builder.build(metadata)
+    end
+
+    test "returns error when remaining_to is not a string" do
+      metadata = %{
+        splits: [
+          %{
+            type: :percent,
+            amount: 20,
+            account: "some:destination"
+          }
+        ],
+        remaining_to: 123,
+        percent_asset: "EUR"
+      }
+
+      assert {:error, "Invalid remaining destination."} = Builder.build(metadata)
+    end
+
+    test "returns error when percent_asset is not a string" do
+      metadata = %{
+        splits: [
+          %{
+            type: :percent,
+            amount: 20,
+            account: "some:destination"
+          }
+        ],
+        percent_asset: 123
+      }
+
+      assert {:error, "Invalid asset."} = Builder.build(metadata)
+    end
+
+    test "returns error when percent_asset is empty" do
+      metadata = %{
+        splits: [
+          %{
+            type: :percent,
+            amount: 20,
+            account: "some:destination"
+          }
+        ],
+        percent_asset: ""
+      }
+
+      assert {:error, "Invalid asset."} = Builder.build(metadata)
+    end
+
+    test "returns error when percent splits exist but percent_asset is missing" do
+      metadata = %{
+        splits: [
+          %{
+            type: :percent,
+            amount: 20,
+            account: "some:destination"
+          }
+        ]
+      }
+
+      assert {:error, "Missing key on metadata."} = Builder.build(metadata)
+    end
+
+    test "returns error when remaining_to is empty" do
+      metadata = %{
+        splits: [
+          %{
+            type: :percent,
+            amount: 20,
+            account: "some:destination"
+          }
+        ],
+        remaining_to: "",
+        percent_asset: "EUR"
+      }
+
+      assert {:error, "Invalid remaining destination."} = Builder.build(metadata)
+    end
+
+    test "returns error when percent amount is negative" do
+      metadata = %{
+        splits: [
+          %{
+            type: :percent,
+            amount: -20,
+            account: "some:destination"
+          }
+        ],
+        percent_asset: "EUR"
+      }
+
+      assert {:error, "Invalid metadata."} = Builder.build(metadata)
+    end
+
+    test "returns error when percent amount is greater than 100" do
+      metadata = %{
+        splits: [
+          %{
+            type: :percent,
+            amount: 120,
+            account: "some:destination"
+          }
+        ],
+        percent_asset: "EUR"
+      }
+
+      assert {:error, "Invalid metadata."} = Builder.build(metadata)
+    end
+
+    test "returns error when percent split has invalid amount" do
+      metadata = %{
+        splits: [
+          %{
+            type: :percent,
+            amount: 101,
+            account: "some:destination"
+          }
+        ],
+        percent_asset: "EUR"
+      }
+
+      assert {:error, "Invalid metadata."} = Builder.build(metadata)
+    end
+
+    test "returns error when fixed split is missing asset" do
+      metadata = %{
+        splits: [
+          %{
+            type: :fixed,
+            amount: 500,
+            account: "some:destination"
+          }
+        ]
+      }
+
+      assert {:error, "Missing key on metadata."} = Builder.build(metadata)
+    end
+
+    test "returns error when fixed split has empty destination" do
+      metadata = %{
+        splits: [
+          %{
+            type: :fixed,
+            amount: 500,
+            asset: "USD",
+            account: ""
+          }
+        ]
+      }
+
+      assert {:error, "Invalid destination."} = Builder.build(metadata)
+    end
+
+    test "returns error when fixed split has empty asset" do
+      metadata = %{
+        splits: [
+          %{
+            type: :fixed,
+            amount: 500,
+            asset: "",
+            account: "some:destination"
+          }
+        ]
+      }
+
+      assert {:error, "Invalid asset."} = Builder.build(metadata)
+    end
+
+    test "returns error when fixed split has negative amount" do
+      metadata = %{
+        splits: [
+          %{
+            type: :fixed,
+            amount: -500,
+            asset: "USD",
+            account: "some:destination"
+          }
+        ]
+      }
+
+      assert {:error, "Invalid metadata."} = Builder.build(metadata)
+    end
+
+    test "returns error when fixed split has zero amount" do
+      metadata = %{
+        splits: [
+          %{
+            type: :fixed,
+            amount: "500",
+            asset: "USD",
+            account: "some:destination"
+          }
+        ]
+      }
+
+      assert {:error, "Invalid metadata."} = Builder.build(metadata)
+    end
   end
 end
